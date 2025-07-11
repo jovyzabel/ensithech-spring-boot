22e40d399bcb6fcb0c8168667a6885d0c8fc26e9
jovyzabel <jovymonka7@gmail.com>
2025-07-11 14:26:51 +0100

    strucutre : src structure

---
+++ src/test/java/com/project/ensitech/EnsitechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ensitech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsitechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/project/ensitech/integration/IntegrationTestBase.java
@@ -0,0 +1,38 @@
+package com.project.ensitech.integration;
+
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Testcontainers
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Transactional
+public abstract class IntegrationTestBase {
+
+    @Container
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test")
+            .withReuse(true); // Réutilise le conteneur entre les tests
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+        registry.add("spring.datasource.driver-class-name", () -> "com.mysql.cj.jdbc.Driver");
+    }
+
+    protected MySQLContainer<?> getMySQLContainer() {
+        return mysql;
+    }
+}
